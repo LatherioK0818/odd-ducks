2946eabb192059cbd39c8994fdfd7b875eded82c
Latherio Kidd <kidd.latherio@gmail.comS>
2023-11-09 23:33:57 -0500

    lab15 b

--- js/app.js
+++ js/app.js
@@ -1,200 +1,168 @@
-'use strict';
-
-// Define your product constructor function
-function Product(name, imagePath) {
-    this.name = name;
-    this.imagePath = imagePath;
-    this.timesShown = 0;
-    this.timesClicked = 0;
-}
-
 
 // Create an array of product objects
 const products = [
-    new Product('bag', 'img/bag.jpg'),
-    new Product('banana', 'img/banana.jpg'),
-    new Product('bathroom', 'img/bathroom.jpg'),
-    new Product('boots', 'img/boots.jpg'),
-    new Product('breakfast', 'img/breakfast.jpg'),
-    new Product('bubblegum', 'img/bubblegum.jpg'),
-    new Product('chair', 'img/chair.jpg'),
-    new Product('cthulhu', 'img/cthulhu.jpg'),
-    new Product('dog-duck', 'img/dog-duck.jpg'),
-    new Product('dragon', 'img/dragon.jpg'),
-    new Product('pen', 'img/pen.jpg'),
-    new Product('pet-sweep', 'img/pet-sweep.jpg'),
-    new Product('scissors', 'img/scissors.jpg'),
-    new Product('shark', 'img/shark.jpg'),
-    new Product('sweep', 'img/sweep.png'),
-    new Product('tauntaun', 'img/tauntaun.jpg'),
-    new Product('unicorn', 'img/unicorn.jpg'),
-    new Product('water-can', 'img/water-can.jpg'),
-    new Product('wine-glass', 'img/wine-glass.jpg')
-];
-//s
-localStorage.setItem('products', JSON.stringify(products));
+    {name: 'bag', imagePath: 'img/bag.jpg'},
+    {name: 'banana', imagePath: 'img/banana.jpg'},
+    {name: 'bathroom', imagePath: 'img/bathroom.jpg'},
+    {name: 'boots', imagePath: 'img/boots.jpg'},
+    {name: 'breakfast', imagePath: 'img/breakfast.jpg'},
+    {name: 'bubblegum', imagePath: 'img/bubblegum.jpg'},
+    {name: 'chair', imagePath: 'img/chair.jpg'},
+    {name: 'cthulhu', imagePath: 'img/cthulhu.jpg'},
+    {name: 'dog-duck', imagePath: 'img/dog-duck.jpg'},
+    {name: 'dragon', imagePath: 'img/dragon.jpg'},
+    {name: 'pen', imagePath: 'img/pen.jpg'},
+    {name: 'pet-sweep', imagePath: 'img/pet-sweep.jpg'},
+    {name: 'scissors', imagePath: 'img/scissors.jpg'},
+    {name: 'shark', imagePath: 'img/shark.jpg'},
+    {name: 'sweep', imagePath: 'img/sweep.png'},
+    {name: 'tauntaun', imagePath: 'img/tauntaun.jpg'},
+    {name: 'unicorn', imagePath: 'img/unicorn.jpg'},
+    {name: 'water-can', imagePath: 'img/water-can.jpg'},
+    {name: 'wine-glass', imagePath: 'img/wine-glass.jpg'}
+  ];
 
-// Retrieve the JSON string from local storage
-const storedProducts = localStorage.getItem('products');
+  let state = {
+    rounds: 20,
+    currentRounds: 0,
+  }
 
-// Parse the JSON string back to an array
-const parsedProducts = JSON.parse(storedProducts);
+  function Product(name, imagePath, timesShown = 0, timesClicked = 0) {
+    this.name = name;
+    this.imagePath = imagePath;
+    this.timesShown = timesShown;
+    this.timesClicked = timesClicked;
+  }
+  let storedProducts = localStorage.getItem('products');
+  const productObjects = storedProducts ? JSON.parse(storedProducts) : products.map(product => new Product(product.name, product.imagePath));
+  
+  const imgElements = document.querySelectorAll('.products');
+  const displayedProducts = [];
+  
+  function getRandomProduct() {
+      let availableProducts = productObjects.filter(product => !displayedProducts.includes(product));
+  
+      if (availableProducts.length === 0) {
+          displayedProducts.length = 0;
+          availableProducts = productObjects.filter(product => !displayedProducts.includes(product));
+      }
+  
+      const randomIndex = Math.floor(Math.random() * availableProducts.length);
+      return availableProducts[randomIndex];
+  }
+  
+  function displayRandomProducts() {
+      for (let i = 1; i <= 3; i++) {
+          const imgElement = document.getElementById(`img_${i}`);
+          const product = getRandomProduct();
+  
+          product.timesShown++;
+          displayedProducts.push(product);
+  
+          imgElement.src = product.imagePath;
+          imgElement.alt = product.name;
+      }
+  }
+  
+  function handleClick(e) {
+      const altText = e.target.alt;
+      const clickedProduct = productObjects.find(({ name }) => name === altText);
+  
+      if (clickedProduct) {
+          clickedProduct.timesClicked++;
+          e.target.dataset.votes = clickedProduct.timesClicked;   
+      }
+  
+      checkState();
+  }
+  
+  function checkState() {
+      if (state.currentRounds >= state.rounds) {
+          const viewResultsButton = document.getElementById('showResults');
+          const imgContainer = document.getElementById('image-container');
+  
+          localStorage.setItem('products', JSON.stringify(productObjects));
+  
+          imgElements.forEach(img => {
+              img.removeEventListener('click', handleClick);
+              if (!img.classList.contains('hidden')) {
+                  img.classList.add('hidden');
+              }
+          });
+  
+          viewResultsButton.classList.remove('hidden');
+          viewResultsButton.addEventListener('click', (e) => {
+              e.preventDefault();
+              displayResults(productObjects);
+              viewResultsButton.classList.add('hidden');
+ 
+          });
+  
+      } else {
+          displayRandomProducts();
+      }
+      state.currentRounds++;
+  }
+  
+  function createChart(imagesArr) {
+      const voteData = imagesArr.map(img => img.timesClicked);
+      const viewData = imagesArr.map(img => img.timesShown);
+      const productNames = imagesArr.map(img => img.name);
+      const ctx = document.getElementById('resultsChart').getContext('2d');
+  
+      new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: productNames,
+              datasets: [
+                  {
+                      label: 'Votes',
+                      data: voteData,
+                      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                      borderColor: 'rgba(75, 192, 192, 1)',
+                      borderWidth: 1
+                  },
+                  {
+                      label: 'Views',
+                      data: viewData,
+                      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                      borderColor: 'rgba(255, 99, 132, 1)',
+                      borderWidth: 1
+                  }
+              ]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+  }
+  
+  function displayResults(imagesArr) {
+      const showResults = document.getElementById('resultsDisplay');
+      let Votes = imagesArr.slice().sort((a, b) => b.timesClicked - a.timesClicked);
+  
+      Votes.forEach(sorted => {
+          let li = document.createElement('li');
+          let subLiVotes = document.createElement('li');
+  
+          li.textContent = `${sorted.name}`;
+          subLiVotes.textContent = `Votes: ${sorted.timesClicked}`;
+  
+          li.appendChild(subLiVotes);
+          showResults.appendChild(li);
+      });
+  
+      createChart(imagesArr);
+  }
+  
+  imgElements.forEach(img => {
+      img.addEventListener('click', handleClick);
+  });
+  
+  checkState();
 
-// Voting Configuration
-let rounds = 25; // Number of voting rounds
-let currentRound = 0;
-let previousProducts = [];
-let allProductsShown = false;
-// DOM Elements
-const productContainer = document.querySelector('.image-container');
-const viewResultsButton = document.getElementById('showResults');
-const resultsDisplay = document.getElementById('resultsDisplay');
-
-// Array to track displayed products
-let displayedProducts = [];
-// Function to create and display the bar chart
-function createBarChart() {
-    if (areVotingRoundsCompleted()) {
-        const voteData = parsedProducts.map(product => product.timesClicked);
-        const viewData = parsedProducts.map(product => product.timesShown);
-        const productNames = parsedProducts.map(product => product.name);
-
-        const ctx = document.getElementById('resultsChart').getContext('2d');
-        console.log(ctx);
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: productNames,
-                datasets: [
-                    {
-                        label: 'Votes',
-                        data: voteData,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Views',
-                        data: viewData,
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    }
-
-}
-
-// Display Products Function
-function displayProducts() {
-    if (currentRound >= rounds) {
-        productContainer.removeEventListener('click', e => handleProductClick(e));
-        viewResultsButton.style.display = 'block';
-        return;
-    }
-
-    if (!allProductsShown) {
-        const uniqueProducts = getUniqueProducts(3, displayedProducts);
-
-        productContainer.innerHTML = '';
-
-        for (const product of uniqueProducts) {
-            const productImage = document.createElement('img');
-            productImage.src = product.imagePath;
-            productImage.alt = product.name;
-            productContainer.appendChild(productImage);
-            product.timesShown++;
-            displayedProducts.push(product);
-        }
-
-        currentRound++;
-
-        if (currentRound >= rounds) {
-            allProductsShown = true;
-        }
-    }
-}
-
-function getUniqueProducts(count, previousProducts) {
-    const uniqueProducts = [];
-    const availableProducts = parsedProducts.slice();
-
-    while (uniqueProducts.length < count && availableProducts.length > 0) {
-        const randomIndex = Math.floor(Math.random() * availableProducts.length);
-        const randomProduct = availableProducts.splice(randomIndex, 1)[0];
-
-        if (!displayedProducts.includes(randomProduct) && !previousProducts.includes(randomProduct)) {
-            uniqueProducts.push(randomProduct);
-        }
-    }
-
-    return uniqueProducts;
-}
-// Get Three Random Unique Products
-function getUniqueProducts(count,previousProducts) {
-    const uniqueProducts = [];
-    const availableProducts = parsedProducts.slice(); // Create a copy of the products array to work with
-
-    while (uniqueProducts.length < count && availableProducts.length > 0) {
-        const randomIndex = Math.floor(Math.random() * availableProducts.length);
-        const randomProduct = availableProducts.splice(randomIndex, 1)[0];
-
-        if (!displayedProducts.includes(randomProduct && !previousProducts.includes(randomProduct))) {
-            uniqueProducts.push(randomProduct);
-        }
-    
-    }
-    return uniqueProducts;
-}
-// Handle Product Click
-function handleProductClick(event) {
-    event.preventDefault();
-    if (event.target.tagName === 'IMG') {
-        const productName = event.target.alt;
-        const product = parsedProducts.find(p => p.name === productName);
-        if (product) {
-            product.timesClicked++;
-            displayProducts();
-        }
-        if (currentRound >= rounds) {
-            viewResultsButton.style.display = 'block';
-        }
-    }
-}
-
-// Event Listener for Product Click
-productContainer.addEventListener('click', e => handleProductClick(e));
-
-// Check if all rounds have been completed
-function areVotingRoundsCompleted() {
-    return currentRound >= rounds;
-}
-
-// Event Listener for Viewing Results
-viewResultsButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    createBarChart();
-    const resultList = document.createElement('ul');
-    resultList.className = 'results-list';
-
-    for (const product of parsedProducts) {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${product.name} had ${product.timesClicked} votes and was seen ${product.timesShown} times.`;
-        resultList.appendChild(listItem);
-    }
-
-    resultsDisplay.appendChild(resultList);
-    viewResultsButton.style.display = 'none';
-});
-
-// Initial Display of Products
-displayProducts();
+  // Ekow Helped me with the corrrections, we had to revamp the whole code.
